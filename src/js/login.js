4f994cbeee654d6452873381af8b9e27025b5725
danicoln <lincolndeveloper@outlook.com>
2023-01-02 18:24:20 -0300

    Criação da página de login

---
+++ src/js/login.js
@@ -0,0 +1,25 @@
+const cardLogin = document.getElementById('card-login');
+const campos = document.querySelectorAll('.required');
+/*const spans = querysellectorall .span-required*/
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/; /* email regex pego da internet*/
+
+function nameValidate(){
+    if(campos[0].value.length < 3){
+        console.log("Nome deve ter 3 caracteres");
+    } 
+    else{
+        console.log("nome validado");
+    }
+}
+
+function logar(){
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login == "admin" && senha == "admin"){
+        alert("Login efetuado com sucesso");
+        location.href = "index.html";
+    } else{
+        alert("Usuário ou senha inválidos!");
+    }
+}
